b42a481ff604eb3a6681b7b3ec697d9562fab248
Marlo Zayro <zayro8905@gmail.com>
2015-05-28 17:57:21 -0500

    se crearon las cabeceras
    se agrego funcion en javascript si existe conexion o no

--- librerias/clase_abstracta.php
+++ librerias/clase_abstracta.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CLASE ABSTRACTA DE DATOS
  * 
@@ -11,11 +12,9 @@
  * @package abstracion
  * @category datos
  */
-
 abstract class datos {
-   #public abstract function operar();
-  
-    protected function local() {
+
+  protected function local() {
     $this->servidores = 'localhost:3308';
     $this->usuarios = 'root';
     $this->claves = 'zayro2014';
@@ -35,9 +34,48 @@ abstract class datos {
     $this->claves = 'zayro2014';
     $this->bdd = 'parqueadero';
   }
-  
 
-    
+  protected function cabecera_cors() {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Method: POST, GET, OPTIONS");
+  }
+
+  protected function cabecera_html() {
+    header("Content-Type:text/html");
+  }
+
+  protected function cabecera_csv() {
+    header("Content-Type: application/csv");
+  }
+
+  protected function cabecera_txt() {
+    header("Content-Type:text/plain");
+  }
+
+  protected function cabecera_pdf() {
+    header("Content-type:application/pdf");
+  }
+
+  protected function cabecera_json() {
+    header('Content-Type: application/json');
+  }
+
+  protected function cabecera_word() {
+    header('Content-type: application/vnd.ms-word');
+    header('Content-Type: application/msword');
+  }
+
+  protected function cabecera_excel() {
+    header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
+    header("Content-type:   application/x-msexcel; charset=utf-8");
+  }
+
+  protected function cabecera_descarga($nombre, $extension) {
+    header("Content-Disposition:attachment;filename='$nombre.$extencion'");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+  }
+
 }
 
 ?>
